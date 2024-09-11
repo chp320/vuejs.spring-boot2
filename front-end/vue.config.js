925d30a549b86fc9175c0a607d833eecf088b80a
chp320 <chp320@naver.com>
2024-09-12 00:37:05 +0900

    spring-boot 의 controller 구성 (/, /login)

--- front-end/vue.config.js
+++ front-end/vue.config.js
@@ -1,6 +1,11 @@
 // webpack-dev-server 가 활용하는 포트를 8080 -> 3000 으로 변경 (프론트엔드, 백엔드 동시 수행 위함)
 module.exports = {
   devServer: {
-    port: 3000
+    port: 3000,
+    proxy: {
+      '/api/*': {
+        target: 'http://localhost:8080'
+      }
+    }
   }
 }
